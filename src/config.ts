2de1836925dded17b5cf2f8629d6a889f2412baa
ShenFri <39716770+ShenFri@users.noreply.github.com>
2023-03-16 22:57:00 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,6 @@ export const config: IConfig = {
   openai_api_key: process.env.OPENAI_API_KEY || "123456789",
   model: process.env.MODEL || "gpt-4.0-turbo",
   chatPrivateTiggerKeyword: process.env.CHAT_PRIVATE_TRIGGER_KEYWORD || "",
-  chatTiggerRule: process.env.CHAT_TRIGGER_RULE || "hi",
+  chatTiggerRule: process.env.CHAT_TRIGGER_RULE || "小G",
   disableGroupMessage: process.env.DISABLE_GROUP_MESSAGE === "true",
 };
