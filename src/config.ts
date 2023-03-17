acc8dbdffcd023b1305e931622cc98af26789bf5
ShenFri <444354821@qq.com>
2023-03-17 15:57:31 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,8 @@ dotenv.config();
 
 export const config: IConfig = {
   openai_api_key: process.env.OPENAI_API_KEY || "123456789",
-  model: process.env.MODEL || "gpt-4.0-turbo",
+  model: process.env.MODEL || "gpt-3.5-turbo",
   chatPrivateTiggerKeyword: process.env.CHAT_PRIVATE_TRIGGER_KEYWORD || "",
-  chatTiggerRule: process.env.CHAT_TRIGGER_RULE || "哥哥",
+  chatTiggerRule: process.env.CHAT_TRIGGER_RULE || "bot",
   disableGroupMessage: process.env.DISABLE_GROUP_MESSAGE === "true",
 };
